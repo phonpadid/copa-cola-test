7614bdb65761805febd3e37bbb78bdc5b6dc16f7
vongsavanh <vongsavanhdev@gmail.com>
2022-12-10 19:02:27 +0700

    create FAQ

--- src/components/GobalLayout/CardFAQ.vue
+++ src/components/GobalLayout/CardFAQ.vue
@@ -1,23 +1,49 @@
 <template>
-  <div class="w-full relative flex flex-col p-8 h-[180px] border-2 border-gray-200">
-    <h1 class="base-title-2">Question ?</h1>
-    <p class="mt-2 text-xl">Answer</p>
-
+  <div class="w-full relative flex flex-col p-8 min-h-[200px] border-2 border-gray-200">
+    <h1 class="text-base font-bold">{{ subStringTitle(faqItem.title) }}</h1>
+    <p class="mt-2 text-sm text-justify">{{ subStringContent(faqItem.body) }}</p>
     <div
-        class="w-[30px] text-white cursor-pointer flex justify-center items-center absolute right-12 h-[30px] bg-blue-500">
+        class="w-[25px] text-white cursor-pointer flex justify-center items-center absolute rounded-full right-12 h-[25px] bg-blue-500">
       <i class="far fa-pencil-alt"></i>
     </div>
     <div
-        class="w-[30px] text-white cursor-pointer flex justify-center items-center absolute  right-2 h-[30px] bg-red-500">
+        class="w-[25px] text-white cursor-pointer flex justify-center items-center absolute rounded-full   right-2 h-[25px] bg-red-500">
       <i class="fas fa-trash"></i>
+    </div>
+    <div class="w-60px absolute bottom-8 right-8">
+      <a-switch class="bg-blue-500" @click="changeStatus" v-model:checked="checked">
+        <template #checkedChildren>
+          <check-outlined/>
+        </template>
+        <template #unCheckedChildren>
+          <close-outlined/>
+        </template>
+      </a-switch>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "CardFAQ"
+<script setup>
+import {ref} from "vue";
+import {CheckOutlined, CloseOutlined} from "@ant-design/icons-vue";
+
+const checked = ref(false);
+const props = defineProps({
+  faqItem: {},
+})
+
+function subStringTitle(text) {
+  if (text) {
+    return text.substring(0, 25) + '...'
+  }
 }
+function subStringContent(text) {
+  if (text) {
+    return text.substring(0, 140) + '...'
+  }
+}
+
+
 </script>
 
 <style scoped>
--- src/views/Pages/FAQ/TabItem/Manage.vue
+++ src/views/Pages/FAQ/TabItem/Manage.vue
@@ -9,11 +9,11 @@
         :label-col='labelCol'
         :wrapper-col='wrapperCol'>
 
-      <a-form-item label="Question">
-        <a-input  size="large"/>
+      <a-form-item label="Question" name="title">
+        <a-input v-model:value="form.title" size="large"/>
       </a-form-item>
-      <a-form-item label="Answer">
-        <a-textarea :rows="6"/>
+      <a-form-item label="Answer" name="body">
+        <a-textarea v-model:value="form.body" :rows="6"/>
       </a-form-item>
       <a-form-item label=' '>
         <a-button class="bg-blue-500" type='primary' @click='onSubmit'>save change</a-button>
@@ -26,11 +26,13 @@
 import {reactive, ref} from 'vue'
 import {NotEmpty} from '@/utils/validate'
 import {useStore} from "vuex";
-import {useRoute} from "vue-router";
+import {useRoute,useRouter} from "vue-router";
 import FAQ from "@/store/models/FAQ.js"
+import {notificationSuccess} from "@/utils/message";
 
 const store = useStore();
 const route = useRoute();
+const router = useRouter();
 const labelCol = {
   xl: 4,
   md: 6,
@@ -43,6 +45,7 @@ const wrapperCol = {
 }
 const ruleForm = ref(null);
 const form = reactive(new FAQ());
+const loading = ref(false);
 const setRef = el => {
   ruleForm.value = el
 }
@@ -50,13 +53,61 @@ const onSubmit = () => {
   ruleForm.value
       .validate()
       .then((res) => {
-        console.log(res)
+        handleSubmit();
       })
       .catch(error => {
       })
 }
+
+
+function handleSubmit() {
+  let uri;
+  let method;
+  uri = `post`;
+  method = "post";
+  const data = JSON.parse(JSON.stringify(form));
+
+  const body = {
+    method: "post",
+    _method: method,
+    actionUri: uri,
+    formData: false,
+    ...data
+  }
+  finalSaveItem(body);
+}
+
+
+function finalSaveItem(body) {
+  loading.value = true;
+  store.dispatch("data-resources/manage", body)
+      .then((res) => {
+        if (res.code === 200) {
+          notificationSuccess({
+            title: "Create Data Successfully",
+            description: "data created !!",
+            position: "topRight"
+          })
+          loading.value = false;
+          router.push({
+            name: "FAQ.index"
+          }).catch(() => {
+
+          })
+        }
+      })
+      .catch((firstErrorBag) => {
+        let error = firstErrorBag.items;
+        loading.value = false
+      }).finally(() => {
+    loading.value = false;
+  })
+}
+
+//validate form
 const rules = {
-  name: [NotEmpty('name')],
+  title: [NotEmpty('title')],
+  body: [NotEmpty('body')],
 };
 </script>
 
