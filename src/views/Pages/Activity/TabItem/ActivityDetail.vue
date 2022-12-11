8ee3b511cf4db0a2d06843658dbcb7f9a880016a
leenaa46 <hsherlock22@gmail.com>
2022-12-11 12:40:48 +0700

    Activity Detail

--- src/views/Pages/Activity/TabItem/ActivityDetail.vue
+++ src/views/Pages/Activity/TabItem/ActivityDetail.vue
@@ -1,54 +1,68 @@
 <template>
   <div class="base-form flex justify-center items-center">
     <div class="w-[800px] min-h-[500px] px-16 pt-4 flex flex-col">
-      <div @click="changeToMainEvent"
-           class="w-full text-blue-600 mb-6 hover:cursor-pointer hover:text-blue-600  text-3xl">
+      <div
+        @click="changeToMainEvent"
+        class="w-full text-blue-600 mb-6 hover:cursor-pointer hover:text-blue-600 text-3xl"
+      >
         <i class="far fa-long-arrow-left"></i>
         <span class="ml-2">Activity Detail</span>
       </div>
       <div class="w-full h-[300px] bg-gray-200">
-        <img class="w-full   h-full object-cover"
-             v-if="activityDetail.image_logo"
-             :src="activityDetail.image_logo.url"
-             alt="">
+        <img
+          class="w-full h-full object-cover"
+          v-if="activityDetail.image_logo"
+          :src="activityDetail.image_logo.url"
+          alt
+        />
       </div>
       <h1 class="text-xl mt-4 font-bold">{{ activityDetail.name }}</h1>
-      <div class="w-[100px] flex items-center justify-center mt-1  h-[20px] base-color-theme">
-        <p class="m-0 p-0 text-xs text-white">
-          01-Jan-2021
-        </p>
+      <div class="w-[100px] flex items-center justify-center mt-1 h-[20px] base-color-theme">
+        <p class="m-0 p-0 text-xs text-white">01-Jan-2021</p>
       </div>
       <p class="text-sm mt-4">{{ activityDetail.description }}</p>
       <div class="w-full flex flex-col min-h-[80px] mt-4">
         <h1 class="text-xl font-bold">Joined</h1>
         <div class="w-full flex h-[40px] mt-4">
           <div class="w-[40px] mr-2 h-[40px] overflow-hidden rounded-full bg-gray-200">
-            <img class="w-full   h-full object-cover"
-                 v-if="activityDetail.image_logo"
-                 :src="activityDetail.image_logo.url"
-                 alt="">
+            <img
+              class="w-full h-full object-cover"
+              v-if="activityDetail.image_logo"
+              :src="activityDetail.image_logo.url"
+              alt
+            />
           </div>
-          <div class="w-[40px] mr-2 h-[40px] overflow-hidden rounded-full bg-gray-200">
-            <img class="w-full   h-full object-cover"
-                 src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                 alt="">
-          </div>
-          <div class="w-[40px] mr-2 h-[40px] overflow-hidden rounded-full bg-gray-200">
-            <img class="w-full   h-full object-cover"
-                 src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-                 alt="">
+          <div
+            v-for="person in activityDetail.personals_active"
+            :key="person"
+            class="w-[40px] mr-2 h-[40px] overflow-hidden rounded-full bg-gray-200"
+          >
+            <img
+              class="w-full h-full object-cover"
+              v-if="person.image_profile"
+              :src="person.image_profile.preview"
+              alt
+            />
           </div>
         </div>
+      </div>
+      <div class="w-full flex flex-col min-h-[80px] mt-4">
+        <h1 class="text-xl font-bold">Past Event</h1>
+        <a-row>
+          <a-col :sm="24" :md="8" :lg="8" v-for="item in activityDetail.posts">
+            <CardEvent :event="item" />
+          </a-col>
+        </a-row>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import {useRouter, useRoute} from "vue-router";
-import {useStore} from "vuex";
+import { useRouter, useRoute } from "vue-router";
+import { useStore } from "vuex";
 import bodyHelpers from "@/utils/BodyHelpers";
-import {onMounted, ref} from "vue";
+import { onMounted, ref } from "vue";
 
 const router = useRouter();
 const route = useRoute();
@@ -57,33 +71,33 @@ const activityDetail = ref({});
 
 function fetchActivityDetail() {
   const id = route.params.activity_id;
-  store.dispatch("data-resources/listing", {
-    actionUri: `variable/${id}`,
-    options_request: bodyHelpers([
-      {type: "activity"},
-    ])
-  }).then((res) => {
-    if (res.code === 200) {
-      activityDetail.value = res.data;
-    }
-  }).catch((error) => {
-    console.log(error)
-  })
+  store
+    .dispatch("data-resources/listing", {
+      actionUri: `variable/${id}`,
+      options_request: bodyHelpers([{ type: "activity" }])
+    })
+    .then(res => {
+      if (res.code === 200) {
+        activityDetail.value = res.data;
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 function changeToMainEvent() {
-  router.push({
-    name: "activity.index"
-  }).catch(() => {
-
-  })
+  router
+    .push({
+      name: "activity.index"
+    })
+    .catch(() => {});
 }
 
 onMounted(() => {
   fetchActivityDetail();
-})
+});
 </script>
 
 <style scoped>
-
 </style>
